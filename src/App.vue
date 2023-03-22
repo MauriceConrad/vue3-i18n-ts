2e950227cabb7ecf64b9b88dad44e7738cdf15f5
Maurice Conrad <conr.maur@gmail.com>
2023-03-23 00:31:26 +0100

    setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>LOL {{t('test')}}</span>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { locale, t } = useI18n({
+  inheritLocale: true
+});
+
+</script>
+
+
+<i18n lang="yaml">
+en:
+  test: Whats up
+de:
+  test: Was geht
+</i18n>
